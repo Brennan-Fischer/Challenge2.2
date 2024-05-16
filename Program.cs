92f26d383730892202f02f1b44297cf5d04bdec5
Brennan <blfische92@gmail.com>
2024-05-16 21:49:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Challenge2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myFile = "C:\\MSSA-CCAD14\\sample.txt";
+            string myDestination = "C:\\MSSA-CCAD14\\test\\sample.txt";
+            Console.WriteLine("Here is the content of " + myFile);
+            using (StreamReader sr = new StreamReader(myFile))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            try
+            {
+                File.Copy(myFile, myDestination, true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.WriteLine("File transfer succesfful to " + myDestination);
+
+            Console.WriteLine("Here is the content of " + myDestination);
+            using (StreamReader sr = new StreamReader(myDestination))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+        }
+    }
+}
